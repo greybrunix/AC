d72b44fa0c214369e207f5c91e480d88caa482f4
greybrunix <a96544@alunos.uminho.pt>
2023-02-03 19:07:46 +0000

    lec_notes

--- C1/notes.c
+++ C1/notes.c
@@ -151,6 +151,130 @@ search(int*v, int a,
  *                 AND
  *                 ((EXI (0<=i<=Na) (EXI (0<=j<=Nb) ai = bj)) => A[p] = B[f])
 */
+
+/*
+ * Hoare triple for While cycles
+ *
+ * P => I  {I AND c} S {I}  (I AND NOT c)=>Q
+ *         {P} while c S {Q}
+
+ * 1. P=>I, Before executing the cycle, I is true
+ * 2. {I AND c} S {I}, Assuming the invariant to be valid before the iteration,
+ *     it must remain valid
+ * 3. (I AND NOT c)=>Q, Q is true when c is false
+*/
+
+/*
+ * Let us consider the factorial function once more;
+  f = 1;
+  while (n>0)
+  { f = f*n;
+    n = n-1;
+  }
+  * a posteriori we can specify this program with:
+  * Pre:: n = n0 AND n0 >= 0
+  * Pos:: (n0 != 0 AND n1 = (n0-1) AND f1 = factorial(n1)*n0 AND f = f1)
+  *       OR (n0 = 0 AND f = f0);
+  * However, how can we assert that such a program is correct?
+  * As seen before, the invariant is the tool for specifying a program with a
+  * while cycle that can be proven to terminate.
+  * Inv:: ALL (0<i<=n) (f(i) = f(i-1)*n(i-1) AND n(i) = n(i-1) - 1)
+*/
+
+/*
+ * Pre: x = x0 AND y = y0 >= 0
+ * Pos: m = x0 * y0
+{ int m = 0, d = y;
+  while (d > 0)
+  { m=m+x;
+    d=d-1;
+  }
+}
+ * Inv: (x=x0 AND y=y0) AND d >= 0 AND x0 * d + m = x0 * y0
+ *
+ * 1.(Pre) => Inv[m\0,d\y]
+ * 2.(Inv AND d > 0) => (Inv[m\m+x,d\d-1])
+ * 3.(Inv AND NOT(d>0)) => (m = x0 * y0)
+ *
+ * 1. Is Inv[m\0,d\y] true? considering y0 >= 0 and x = x0?
+ *    Yes! Let us analyse;
+ *    (x = x0 AND y = y0) AND y >= 0 AND x0 * y + 0 = x0 * y0
+ * ===(x = x0 AND y = y0) AND y0 >= 0 AND x0 * y0 = x0 * y0
+ * 2. Is Inv[m\m+x,d\d-1]) True? knowing that d > 0?
+ *    Yes! Let us analyse;
+ *    (x = x0 AND y = y0) AND d >= 0 AND x0 * d + m = x0 * y0 AND d > 0
+ *  =>(x = x0 AND y = y0) AND d > 0 AND x0 * d + m = x0 * y0
+ *  =>(x = x0 AND y = y0) AND d >= 1 AND x0 * (d-1) + m + ? = x0 * y0
+ *  =>(x = x0 AND y = y0) AND d-1>=0 AND x0 * (d-1) + (m+x0) = x0*y0
+ * 3. Is (m = x0 * y0)? True when d = 0?
+ *    (x = x0 AND y = y0) AND d >= 0 AND x0*d+m=x0+y0 AND d <= 0
+ *  =>(x = x0 AND y = y0) AND d = 0 AND x0* d+m=x0+y0
+ *  =>(x = x0 AND y = y0) AND d = 0 AND m = x0 + y0
+*/
+
+/* Total Correction
+ *
+ * Variant
+ *    [P] S [Q]
+ * 1. P is valid then S terminates
+ * 2. From all states where P is valid, then S terminates, reaching states
+ * where Q is valid.
+ *
+ * How can we assert the termination of a cycle however?
+*/
+
+/* VARIANT
+ * The variant is an integer expression that
+ * 1. decreases in each iteration
+ * 2. is always greater than a pre-defined value (typically 0)
+*/
+
+/* Exercise
+ *
+ * Pre:: ALL (0<=i<N) A[i] = a(i)
+ * Pos:: s = SUM (i=0;N-1) a(i)
+ *
+ * 1. 
+s = 0; p = 0;
+while (p<N)
+{ s = s + A[p];
+  p = p + 1;
+}
+ * Invariant
+ * Pre => Inv[s\0,p\0]
+ * (Inv AND (p<N)) => V >= 0
+ * (Inv AND (p<N) AND V = v0) => (Inv AND Var < v(0))[s\s+A[p],p\p+1]
+ * (Inv AND (p>=N)) => Pos
+ *
+ *  Inv = ALL (0<=i<N) A[i] = a(i) AND s = SUM (0;p-1) a(p-1)
+ *  Var: N-p (>0)
+ *
+ *   ALL (0<=i<N) A[i] = a(i) => (0 = SUM(0;-1) a(-1) AND Pre)
+ * =>ALL (0<=i<N) A[i] = a(i) => (0 = 0 AND Pre)
+ * =>True
+ *
+ *   (s = SUM (0;p) a(p) AND Pre AND (p<N)) => N-p > 0
+ * =>   (p0 and Pre AND 0<N-p) => N-p > 0
+ * => TRUE
+ *   s = SUM (0;p) a(p) AND Pre AND (p<N) AND Var = v(0)
+ * => Pre AND s+A[p] = SUM (0;p+1) a(p+1) AND (N-(p+1)) < N-p
+ * => TRUE AND SUM (0;p) a(p)+A[p] = SUM (0;p+1) a(p+1) AND
+                                              (N-(p+1)) < N-p
+ * => TRUE AND SUM (0;p+1) a(p+1) = SUM (0;p+1) a(p+1)
+                                           AND (N-(p+1)) < N-p
+ * => TRUE AND TRUE AND (N-(p+1)) < N-p
+ * => TRUE AND TRUE AND (N-p-1) < N-1
+ * => TRUE AND TRUE AND TRUE
+ * => TRUE
+ *  (Inv AND (p>=N)) => Pos
+ * =>Pre AND s = SUM (0;p) a(p) AND p>=N AND N = p
+ *  Note how if p is greater or equal than N it will be greater than N-1
+ * =>Pre AND s = SUM (0;p) a(p) AND p > N-1 AND N = p
+ *   => s = SUM (0,N-1) a(i)
+ * =>TRUE
+ *
+*/
+
 int
 main()
 { return 0;
